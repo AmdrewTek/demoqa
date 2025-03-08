87d1374dea6b7f566601f4aedd95e81b529c974c
AndrewTek <tekunov.andrei@gmail.com>
2025-03-08 14:40:55 +0300

    Исправил тест. Добавил закрытие баннеров Использовал более корткий способ загрузки файла. Добавил проверку теста. Исправил значения подаваемые в форму "номер телефона".

--- src/test/java/DemoQA.java
+++ src/test/java/DemoQA.java
@@ -4,14 +4,14 @@ import org.junit.jupiter.api.Test;
 
 import java.io.File;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
-public class demoQA {
+public class DemoQA {
 
     @BeforeAll
-        static void BeforeAll() {
+        static void beforeAll() {
             //Configuration.browser = "Edge";
             Configuration.pageLoadStrategy = "eager";
             Configuration.browserSize = "1920x2000";
@@ -19,21 +19,24 @@ public class demoQA {
         }
 
     @Test
-    void FormTest() {
+    void formTest() {
         open("/automation-practice-form");
+        executeJavaScript("$('#fixedban').remove()");//Закрывают баннеры
+        executeJavaScript("$('footer').remove()");
         $("#firstName").setValue("Andrew");
         $("#lastName").setValue("Tek");
         $("#userEmail").setValue("andrew@exp.com");
         $(byText("Male")).click();
-        $("#userNumber").setValue("88005553535");
+        $("#userNumber").setValue("1234567890");
         $("#dateOfBirthInput").click();
         $(".react-datepicker__year-select").selectOption("2024");
         $(".react-datepicker__month-select").selectOption("October");
         $(".react-datepicker__month").$(byText("9")).click();
         $("#subjectsInput").setValue("Computer Science").pressEnter();
         $(byText("Sports")).click();
-        File fileToUpload = new File("src/test/resources/file.jpg");
-        $("#uploadPicture").uploadFile(fileToUpload);
+        $("#uploadPicture").uploadFromClasspath("file.jpg"); //более короткий способ
+        //File fileToUpload = new File("src/test/resources/file.jpg");
+        //$("#uploadPicture").uploadFile(fileToUpload);
         $("#currentAddress").setValue("st.Pushkin");
         $("#state").click();
         $(byText("NCR")).click();
@@ -41,5 +44,15 @@ public class demoQA {
         $(byText("Delhi")).click();
         $("#submit").click();
 
+        $(".modal-body").shouldHave(text("Andrew Tek"));
+        $(".modal-body").shouldHave(text("andrew@exp.com"));
+        $(".modal-body").shouldHave(text("Male"));
+        $(".modal-body").shouldHave(text("1234567890"));
+        $(".modal-body").shouldHave(text("09 October,2024"));
+        $(".modal-body").shouldHave(text("Computer Science"));
+        $(".modal-body").shouldHave(text("Sports"));
+        $(".modal-body").shouldHave(text("file.jpg"));
+        $(".modal-body").shouldHave(text("st.Pushkin"));
+        $(".modal-body").shouldHave(text("NCR Delhi"));
     }
 }
